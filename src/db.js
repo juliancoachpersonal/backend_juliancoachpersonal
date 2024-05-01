e59b47d072f266a35a0f53e73bae3094d9f755e4
FCristanchoBOCC <fcristancho@bancodeoccidente.com.co>
2024-05-01 07:43:14 -0500

    Redeploy node

--- src/db.js
+++ src/db.js
@@ -6,6 +6,7 @@ dotenv.config();
 export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.DB_CONNECTION_STRING)
+        
         // await mongoose.connect('mongodb://127.0.0.1/julian-coach-personal')
         console.log('DB conected successfully!')
     } catch (error) {
